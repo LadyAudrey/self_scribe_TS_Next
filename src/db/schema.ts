ef468b2110d3c467bdfbb6bcfe797b37d42d57a3
LadyAudrey <theaudreygreene@gmail.com>
2024-10-25 09:07:07 -0700

    docker bug

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+// where you define the shape of the DB tables etc in JS
+
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
