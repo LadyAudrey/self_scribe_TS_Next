5f0fb61c5acf2c41b9f5bcdf9cab16bc7a5d114b
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-10-25 15:05:26 -0500

    Initial Drizzle setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,17 @@
-// where you define the shape of the DB tables etc in JS
-
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { sql, SQL } from "drizzle-orm";
+import { AnyPgColumn, integer, pgTable, uniqueIndex, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+  email: varchar({ length: 255 }).notNull()
+},
+  (table) => {
+    // Enforces both, lowcasing and unique contraint
+    return { emailUniqueIndex: uniqueIndex("emailUniqueIndex").on(lower(table.email)) }
+  }
+);
+
+export function lower(email: AnyPgColumn): SQL {
+  return sql`lower(${email})`
+}
