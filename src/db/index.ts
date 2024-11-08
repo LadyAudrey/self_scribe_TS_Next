e83df2d7270ebdc47713b3239086c3648b2e8ff7
LadyAudrey <theaudreygreene@gmail.com>
2024-11-08 08:20:17 -0800

    debugging

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,23 @@
-import { sql } from "@vercel/postgres"
-import postgres from "postgres"
-import { drizzle as VercelDrizzle, VercelPgDatabase } from "drizzle-orm/vercel-postgres";
-import { drizzle as LocalDrizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js"
+console.log(process.env.POSTGRES_URL, "process.envPOSTGRES_URL");
+import { sql } from "@vercel/postgres";
+import postgres from "postgres";
+import {
+  drizzle as VercelDrizzle,
+  VercelPgDatabase,
+} from "drizzle-orm/vercel-postgres";
+import {
+  drizzle as LocalDrizzle,
+  PostgresJsDatabase,
+} from "drizzle-orm/postgres-js";
 import assert from "assert";
-
-assert(process.env.POSTGRES_URL, "environment variable POSTGRES_URL not set")
-let db: VercelPgDatabase | PostgresJsDatabase
+assert(!!process.env.POSTGRES_URL, "environment variable POSTGRES_URL not set");
+let db: VercelPgDatabase | PostgresJsDatabase;
 
 if (process.env.NODE_ENV === "production") {
-  db = VercelDrizzle(sql)
+  db = VercelDrizzle(sql);
 } else {
-  const migrationClient = postgres(process.env.POSTGRES_URL!)
-  db = LocalDrizzle(migrationClient)
+  const migrationClient = postgres(process.env.POSTGRES_URL!);
+  db = LocalDrizzle(migrationClient);
 }
 
-export { db }
+export { db };
