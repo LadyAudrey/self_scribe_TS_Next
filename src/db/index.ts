0052ef49699ad810d3083b66be3affaed933e179
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-18 10:40:34 -0600

    Remove local drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,24 +1,10 @@
 import { sql } from "@vercel/postgres";
-import postgres from "postgres";
-import {
-  drizzle as VercelDrizzle,
-  VercelPgDatabase,
-} from "drizzle-orm/vercel-postgres";
-import {
-  drizzle as LocalDrizzle,
-  PostgresJsDatabase,
-} from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 if (!process.env.POSTGRES_URL) {
   throw new Error("environment variable POSTGRES_URL not set");
 }
-let db: VercelPgDatabase | PostgresJsDatabase;
 
-if (process.env.NODE_ENV === "production") {
-  db = VercelDrizzle(sql);
-} else {
-  const migrationClient = postgres(process.env.POSTGRES_URL!);
-  db = LocalDrizzle(migrationClient);
-}
+const db = drizzle(sql);
 
 export { db };
