ea6f7164eeec9259f3729b0d06c0b466978cd39a
LadyAudrey <theaudreygreene@gmail.com>
2025-01-01 19:31:52 -0800

    working on Symptoms UI creation

---
+++ src/app/dashboard/data/page.tsx
@@ -0,0 +1,9 @@
+import Side from "@/components/UI/Side";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import { eq, desc } from "drizzle-orm";
+
+
+export default function page() {
+  return <div>page</div>;
+}
