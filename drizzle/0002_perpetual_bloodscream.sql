e3f4744612e0ac2771c3b2ac9c99e90b53fda7b3
LadyAudrey <theaudreygreene@gmail.com>
2025-02-17 17:33:55 -0800

    repaired schema file

--- drizzle/0002_perpetual_bloodscream.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "lists" RENAME COLUMN "listId" TO "id";--> statement-breakpoint
-ALTER TABLE "lists" ALTER COLUMN "id" SET DATA TYPE text;
--- drizzle/0005_skinny_christian_walker.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "tasks" ALTER COLUMN "frequency" SET NOT NULL;
