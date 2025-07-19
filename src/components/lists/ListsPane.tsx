e2b019d7dee1d89f4b462e2f581eda63cbf2539e
LadyAudrey <theaudreygreene@gmail.com>
2025-07-18 18:41:11 -0700

    added accordion to Lists UI

--- src/components/lists/ListsPane.tsx
+++ src/components/lists/ListsPane.tsx
@@ -7,6 +7,12 @@ import { EditItem } from "../UI/EditItem";
 import { Tasks } from "../tasks/Tasks";
 import { AddTask } from "../tasks/AddTask";
 import { Lists } from "@/app/dashboard/lists/page";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../UI/Accordion";
 
 type ListsProps = {
   toDoLists: Lists;
@@ -33,15 +39,22 @@ export async function ListsPane({ toDoLists }: ListsProps) {
           <li key={list.listId} className="flex flex-col gap-2">
             <div className="flex justify-between gap-2">
               <EditItem name={list.name} updateItem={updateList} />
+              <Accordion type={"single"} collapsible>
+                <AccordionItem value={"ddfaa"}>
+                  <AccordionTrigger className="flex grow justify-between items-center gap-2"></AccordionTrigger>
+                  <AccordionContent className="flex justify-between">
+                    <div className="flex flex-col gap-2 ps-4">
+                      <Tasks tasks={list.tasks} completed={false} />
+                      <AddTask listId={list.listId} />
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
               <DeleteBtn
                 deleteFn={deleteList}
                 confirmationTxt="Are you sure you want to delete this list? All of your tasks will be permanently removed. This action cannot be undone."
                 modalTitle="Delete List"
               />
-            </div>
-            <div className="flex flex-col gap-2 ps-4">
-              <Tasks tasks={list.tasks} completed={false} />
-              <AddTask listId={list.listId} />
             </div>
           </li>
         );
